7850ed7031f69aaec8e55634cd206d5ca0a8f39a
Hamish B <hmblair@stanford.edu>
2024-03-24 18:05:16 -0700

    Added the FAPE loss

--- loss.py
+++ loss.py
@@ -1,5 +1,6 @@
 
 import torch
+import torch.nn as nn
 
 def kabsch_align(x : torch.Tensor, y : torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
     """
@@ -28,8 +29,8 @@ def kabsch_align(x : torch.Tensor, y : torch.Tensor) -> tuple[torch.Tensor, torc
     """
 
     # center the point clouds
-    x = x - x.mean(1, keepdim=True)
-    y = y - y.mean(1, keepdim=True)
+    x = x - x.mean(-2, keepdim=True)
+    y = y - y.mean(-2, keepdim=True)
 
     # calculate the covariance matrix
     C = torch.einsum('...ij,...ik->...jk', x, y)
@@ -46,16 +47,12 @@ def kabsch_align(x : torch.Tensor, y : torch.Tensor) -> tuple[torch.Tensor, torc
 
 
 
-class KabschLoss(torch.nn.Module):
+class KabschLoss(nn.Module):
     """
     The Kabsch loss function, which calculates the mean square deviation
     between two point clouds after applying the Kabsch algorithm to optimally
     align the point clouds.
     """
-    def __init__(self):
-        super().__init__()
-
-
     def forward(self, x : torch.Tensor, y : torch.Tensor) -> torch.Tensor:
         """
         Calculate the mean square deviation between two point clouds, after
@@ -74,8 +71,83 @@ class KabschLoss(torch.nn.Module):
             A tensor of shape (1,) representing the mean square deviation
             between the two point clouds.
         """
+
         # align the point clouds
         x, y = kabsch_align(x, y)
 
         # calculate the mean square deviation
         return ((x - y) ** 2).mean()
+    
+
+
+def frame_align(x : torch.Tensor, R : torch.Tensor) -> torch.Tensor:
+    """
+    Take a point cloud x in R^d and corresponding orientations R, and align the point
+    cloud at each point using the rotation matrix at that point. The point cloud
+    is also centered at the origin.
+
+    Parameters:
+    -----------
+    x : torch.Tensor
+        A tensor of shape (..., N, d) representing the point cloud.
+    R : torch.Tensor
+        A tensor of shape (..., N, d, d) representing the frame rotation matrix 
+        at each point in the point cloud.
+
+    Returns:
+    --------
+    torch.Tensor
+        A tensor of shape (..., N, N, d) representing the point cloud aligned at
+        each point using the rotation matrix at that point. Dimension -d 
+        corresponds to which frame was used to align the point cloud, and 
+        dimension -d corresponds to the point in the point cloud after alignment.
+    """
+
+    # center the point cloud
+    x = x - x.mean(-2, keepdim=True)
+
+    # invert the rotation matrices by transposing them
+    R_inv = R.transpose(-2, -1)
+
+    # align the point cloud at each point separately
+    return torch.einsum('...lij,...kj->...lki', R_inv, x)
+
+
+
+class FrameAlignedPointError(nn.Module):
+    """
+    For copmuting the Frame Aligned Point Error (FAPE) between two oriented point
+    clouds.
+    """
+    def forward(
+            self, 
+            x : torch.Tensor, 
+            R_x : torch.Tensor, 
+            y : torch.Tensor, 
+            R_y : torch.Tensor,
+            ) -> torch.Tensor:
+        """
+        Compute the NxN matrix of frame-aligned distances between the two point
+        clouds in R^d, and return the mean such distance. 
+
+        Parameters:
+        -----------
+        x : torch.Tensor
+            A tensor of shape (..., d, 3) representing the first point cloud.
+        R_x : torch.Tensor
+            A tensor of shape (..., N, d, d) representing the orientation of each
+            point in the first point cloud, stored as a rotation matrix.
+        y : torch.Tensor
+            A tensor of shape (..., N, d) representing the second point cloud.
+        R_y : torch.Tensor
+            A tensor of shape (..., N, d, d) representing the orientation of each
+            point in the second point cloud, stored as a rotation matrix.
+        """
+
+        # align the point clouds according to their rotations
+        x_aligned = frame_align(x, R_x)
+        y_aligned = frame_align(y, R_y)
+
+        # calculate the frame-aligned point error, which is the mean squared
+        # distance between the two point clouds
+        return ((x_aligned - y_aligned) ** 2).mean()
